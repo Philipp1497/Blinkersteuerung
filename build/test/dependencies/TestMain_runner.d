433030fe0a70a21f69317adbaf4623b3c7e73c2e
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-08-19 20:19:04 +0200

    Ceedling files

---
+++ build/test/dependencies/TestMain_runner.d
@@ -0,0 +1,17 @@
+build/test/out/c/TestMain_runner.o: build/test/runners/TestMain_runner.c \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/cmock/src/cmock.h \
+ C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/cmock/src/cmock_internals.h \
+ build/test/mocks/MockInit.h Source/src/Init.h \
+ Source/src/DSP280x_Device.h Source/src/DSP280x_Adc.h \
+ Source/src/DSP280x_DevEmu.h Source/src/DSP280x_CpuTimers.h \
+ Source/src/DSP280x_ECan.h Source/src/DSP280x_ECap.h \
+ Source/src/DSP280x_EPwm.h Source/src/DSP280x_EQep.h \
+ Source/src/DSP280x_Gpio.h Source/src/DSP280x_I2c.h \
+ Source/src/DSP280x_PieCtrl.h Source/src/DSP280x_PieVect.h \
+ Source/src/DSP280x_Spi.h Source/src/DSP280x_Sci.h \
+ Source/src/DSP280x_SysCtrl.h Source/src/DSP280x_XIntrupt.h \
+ Source/src/DSP280x_Examples.h Source/src/DSP280x_GlobalPrototypes.h \
+ Source/src/DSP280x_ePwm_defines.h Source/src/DSP280x_I2C_defines.h \
+ Source/src/DSP280x_DefaultISR.h Source/src/Timer.h Source/src/Taster.h
