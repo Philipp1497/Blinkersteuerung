ca273f45b73d8ea5a5bb7c7f073eecb6d803284c
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-08-22 11:20:41 +0200

    Ceedling files

--- Tools/splint/start.bat
+++ Tools/splint/start.bat
@@ -2,9 +2,9 @@
 
 set defines=-DSPLINT -Dinterrupt=""
 
-splint.exe C:\Blinkersteuerung\Source\src\Main.c %defines% >> log.txt 2>&1
-splint.exe C:\Blinkersteuerung\Source\src\Taster.c %defines% >> log.txt 2>&1
-splint.exe C:\Blinkersteuerung\Source\src\Timer.c %defines% >> log.txt 2>&1
-splint.exe C:\Blinkersteuerung\Source\src\Init.c %defines% >> log.txt 2>&1
+splint.exe Main.c %defines% >> log.txt 2>&1
+splint.exe Taster.c %defines% >> log.txt 2>&1
+splint.exe Timer.c %defines% >> log.txt 2>&1
+splint.exe Init.c %defines% >> log.txt 2>&1
 exit 0
 pause
